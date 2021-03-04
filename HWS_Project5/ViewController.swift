086be51ecb96e019f3f41f8f932d1bf04a1e5e6c
nofprog <k.jin3217@gmail.com>
2021-03-04 23:10:28 +0900

    initial commit, start.txtの追加, キャスト

--- HWS_Project5/ViewController.swift
+++ HWS_Project5/ViewController.swift
@@ -7,11 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //start.txtの場所を指定
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            
+            //startWordsURLに格納されたファイルをString型にキャスト
+            //try? → エラーが投げられたらnilを返す
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                
+                //改行(\"n")が見つかったら文字列を分割して配列に入れる
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+
+
+    }
+    
+    //すべての文字列から1つのランダムなアイテムを選択
+    func startGame(){
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+        
     }
 
 
